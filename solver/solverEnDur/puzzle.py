bcc0cc18160963a7056f79a8454599cfc6d35f07
BuildTools <unconfigured@null.spigotmc.org>
2018-06-04 19:07:43 +0200

    Insertion of the solver for the other grid Only achieved to get 1024, and sometimes 512

--- solver/solverEnDur/puzzle.py
+++ solver/solverEnDur/puzzle.py
@@ -26,6 +26,12 @@ KEY_DOWN = "'s'"
 KEY_LEFT = "'q'"
 KEY_RIGHT = "'d'"
 
+MERGE_FUNCTIONS = {
+    'left': left,
+    'right': right,
+    'up': up,
+    'down': down
+}
 
 class GameGrid(Frame):
     def __init__(self):
@@ -44,6 +50,7 @@ class GameGrid(Frame):
         self.init_matrix()
         self.update_grid_cells()
 
+        b = self.grid_cells
         #self.mainloop()
 
     def init_grid(self):
@@ -116,6 +123,19 @@ class GameGrid(Frame):
         while self.matrix[index[0]][index[1]] != 0:
             index = (self.gen(), self.gen())
         self.matrix[index[0]][index[1]] = 2
+        
 
+    def actions(self):
+        """ Generate the subsequent board after moving """
+
+        def moved(matrix, t):
+            return any(x != y for x, y in zip(matrix, t)) 
+        
+        """ Determine quels mouvements sont possibles : retourne un tuple avec l'action realisee et 't' etant la grille resultant du mouvement effectue. """
+        for action, f in [("left",left), ("down",down), ("up",up), ("right",right)]:
+            t = f(self.matrix)
+            """Si il s'est passe quelque chose, on envoie, sinon non"""
+            if moved(self.matrix, t[0]):
+                yield action, t
 
 #gamegrid = GameGrid()
--- solver/solverEnDur/testSolver.py
+++ solver/solverEnDur/testSolver.py
@@ -2,5 +2,5 @@ from solver import *
 
 maFen = GameGrid()
 Solver = IA()
-#Solver.simulation(maFen)
+maFen.after(2000, Solver.simulation(matrix = maFen.matrix, maFen = maFen))
 maFen.mainloop()
